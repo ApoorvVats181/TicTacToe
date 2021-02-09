ca1d121c22da354bf1c7e4f79479cbc70da019a8
Apoorv Vats <apoorvvats181@gmail.com>
2021-02-09 16:46:18 +0530

    added sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import Board from "./components/Board"
-export default () => (
-  <div>
-    <h1>TIC TAC TOE</h1>
-    <Board />
-  </div>
-);
+import Board from "./components/Board";
+
+import "./styles/root.scss";
+
+const App = () => { 
+return (
+    <div className="app">
+      <h1>TIC TAC TOE</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,21 @@ import Square from './Square'
 
 const Board = () => {
   return (
-    <div>
+    <div className="board">
 
-      <div>
+      <div className="board-row">
         <Square value={0}> </Square>
         <Square value={1}> </Square>
         <Square value={2}> </Square>        
       </div>
 
-      <div>
+      <div className="board-row">
         <Square value={3}> </Square>
         <Square value={4}> </Square>
         <Square value={5}> </Square>
       </div>
 
-      <div>
+      <div className="board-row">
         <Square value={6}> </Square>
         <Square value={7}> </Square>
         <Square value={8}> </Square>
