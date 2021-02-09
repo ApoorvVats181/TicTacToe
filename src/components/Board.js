0a6077e6f0b4555566f6598d93391612b544eb7a
Apoorv Vats <apoorvvats181@gmail.com>
2021-02-09 13:34:09 +0530

    board and square components

---
+++ src/components/Board.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = () => {
+  return (
+    <div>
+
+      <div>
+        <Square value={0}> </Square>
+        <Square value={1}> </Square>
+        <Square value={2}> </Square>        
+      </div>
+
+      <div>
+        <Square value={3}> </Square>
+        <Square value={4}> </Square>
+        <Square value={5}> </Square>
+      </div>
+
+      <div>
+        <Square value={6}> </Square>
+        <Square value={7}> </Square>
+        <Square value={8}> </Square>
+      </div>
+
+    </div>
+  )
+}
+
+export default Board
+
+
